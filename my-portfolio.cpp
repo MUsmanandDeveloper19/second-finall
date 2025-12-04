00a6c93d757e4c605ee7bf45a2d4c662f6ab6f0d
MUsmanHabib <musmanchauhan191@gmail.com>
2025-12-04 06:24:12 +0500

    Add files via upload

---
+++ my-portfolio.cpp
@@ -0,0 +1,10 @@
+my-portfolio/
+│── index.html
+│── confirmation.html
+│── my-portfolio/confirmation.html
+│
+└── assets/
+      ├── css/
+      │     └── style.css     (optional)
+      └── js/
+            └── main.js       (optional)
